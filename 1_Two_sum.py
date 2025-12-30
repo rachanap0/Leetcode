db29ba5f347e9ac7b0664c28e4a9c89b8aadc763
Rachana Pandey <rachanapandey@Rachanas-MacBook-Pro.local>
2025-12-29 22:57:17 -0800

    updated code

--- 1_Two_sum.py
+++ 1_Two_sum.py
@@ -1,6 +1,6 @@
-#correct
 
-# TC: O(n^2) where n is number of elements in the array
+# Brute force approach
+# # TC: O(n^2) where n is number of elements in the array
 
 class Solution(object):
     def twoSum(self, nums, target):
@@ -21,3 +21,49 @@ solution = Solution()
 nums = [2,7,11,15]
 target = 9
 print(solution.twoSum(nums, target))  # Output: [0, 1]
+
+
+# nums = [2,7,11,15]
+# target = 9
+# 2 -> 0
+# 7 -> 1
+# 11 ->3
+# 15 -> 4
+
+# Optimized approach using a hash map 
+# TC: O(n) where n is number of elements in the array
+class Solution(object):
+    def twoSum(self, nums, target):
+        dict = {}
+
+        for i, n in enumerate(nums):
+            dict[n] = i
+        
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in dict and dict[diff] != i:
+                return [i, dict[diff]]
+        return []
+    
+solution = Solution()
+nums = [2,7,11,15]
+target = 9
+print(solution.twoSum(nums,target))
+
+# Further optimized approach using a single pass hash map
+# TC: O(n) where n is number of elements in the array
+class Solution:
+    def twoSum(self, nums, target):
+        dict ={}
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in dict:
+                return[dict[diff], i]
+            dict[n] = i
+        return []
+    
+solution = Solution()
+nums = [4,5,6]
+target = 10
+print(solution.twoSum(nums, target))
