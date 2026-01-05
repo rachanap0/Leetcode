e33edd94907f39564d9c40ffda411fab80c7d31b
Rachana Pandey <rxp3076@mavs.uta.edu>
2026-01-04 19:01:32 -0800

    changes to 3sum

--- 15_3Sum.py
+++ 15_3Sum.py
@@ -1,4 +1,4 @@
-# Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k.
+# Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]]that sum to 0 such that i != j, i != k, and j != k.
 # Notice that the solution set must not contain duplicate triplets.
 # Example 1:
 # Input: nums = [-1,0,1,2,-1,-4]
