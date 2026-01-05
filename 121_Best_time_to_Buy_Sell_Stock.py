86ec9060b895ce0d58f5cc2e9a4fb029190078c9
Rachana Pandey <rxp3076@mavs.uta.edu>
2026-01-04 19:22:02 -0800

    updated problem 121

--- 121_Best_time_to_Buy_Sell_Stock.py
+++ 121_Best_time_to_Buy_Sell_Stock.py
@@ -2,6 +2,8 @@
 # You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
 # Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit,
 # return 0.
+#TC: O(n)
+#SC: O(1)
 
 def maxProfit(prices):
     buy = prices[0]
@@ -17,3 +19,24 @@ def maxProfit(prices):
 #example usage
 prices = [7,1,5,3,6,4]
 print(maxProfit(prices))  # Output: 5
+
+#Optimized approach
+# TC: O(n)
+# SC: O(1)
+def maxProfit_pointer(prices):
+    buy = 0
+    sell = 1
+    max_profit =0
+
+    while sell <len(prices):
+        if prices[buy] < prices[sell]:
+            profit = prices[sell] - prices[buy]
+            max_profit = max(profit, max_profit)
+        else:
+            buy = sell
+        sell +=1
+    return max_profit
+
+# #example usage
+prices = [7,1,5,3,6,4]
+print(maxProfit_pointer(prices))  # Output: 5
